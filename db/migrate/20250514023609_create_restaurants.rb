71f90eea4b5ed76d9f1b7ab84fe6bedcd4b198cc
Kaho Maeda <kaho@tutamail.com>
2025-05-14 11:52:03 +0900

    models created

--- db/migrate/20250514023609_create_restaurants.rb
+++ db/migrate/20250514023609_create_restaurants.rb
@@ -2,6 +2,9 @@ class CreateRestaurants < ActiveRecord::Migration[7.1]
   def change
     create_table :restaurants do |t|
       t.string :name
+      t.string :address
+      t.string :phone_number
+      t.string :category
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,16 @@
 ActiveRecord::Schema[7.1].define(version: 2025_05_14_023652) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
+    t.string "address"
+    t.string "phone_number"
+    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
